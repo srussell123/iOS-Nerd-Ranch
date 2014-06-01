373826057540d8a2f797bf583520eabf41ce26e4
srussell123 <scottbrussell@gmail.com>
2014-06-01 19:14:15 +0900

    Ch1
    Quiz program:
    Very basic UI practice and familiarization with Xcode.
    Also connected developer account and tested on iPhone.
    -Might look into init issue with array++ in questionButton action method

---
+++ Quiz/Quiz/BNRQuizViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRQuizViewController.h
+//  Quiz
+//
+//  Created by Scott Russell on 6/1/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRQuizViewController : UIViewController
+
+@end
