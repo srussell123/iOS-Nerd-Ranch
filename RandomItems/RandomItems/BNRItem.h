c98c696f448e0e01a972d003faaf7c8d9cd1f3ef
srussell123 <scottbrussell@gmail.com>
2014-06-02 13:14:51 +0900

    RandomItems
    Ch 2:
    RandomItems project working with basic console and object creation in
    Obj-C.
    Instantiate randomItems objects, fill array , NSLog using an overridden
    (NSString *)description method

---
+++ RandomItems/RandomItems/BNRItem.h
@@ -0,0 +1,38 @@
+//
+//  BNRItem.h
+//  RandomItems
+//
+//  Created by Scott Russell on 6/1/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRItem : NSObject
+{
+    NSString *_itemName;
+    NSString *_serialNumber;
+    int _valueInDollars;
+    NSDate *_dateCreated;
+}
+
++ (instancetype)randomItem;
+
+- (instancetype)initWithItemName: (NSString *)name
+                  valueInDollars: (int)value
+                    serialNumber: (NSString *)sNumber;
+
+- (instancetype)initWithItemName: (NSString *)name;
+
+- (void) setItemName:(NSString *)str;
+- (NSString *)itemName;
+
+- (void)setSerialNumber:(NSString *)str;
+- (NSString *)serialNumber;
+
+- (void)setValueInDollars:(int)v;
+- (int)valueInDollars;
+
+- (NSDate *)dateCreated;
+
+@end
---
+++ RandomItems/RandomItems/main.m
@@ -0,0 +1,33 @@
+//
+//  main.m
+//  RandomItems
+//
+//  Created by Scott Russell on 6/1/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "BNRItem.h"
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        //Create a mutable array object, store its address in items var
+        NSMutableArray *items = [[NSMutableArray alloc] init];
+        
+        for (int i = 0; i < 10; i++) {
+            BNRItem *item = [BNRItem randomItem];
+            [items addObject:item];
+        }
+        
+        for (BNRItem *item in items) {
+            NSLog(@"%@", item);
+        }
+        
+        items = nil;
+    }
+    return 0;
+}
+
