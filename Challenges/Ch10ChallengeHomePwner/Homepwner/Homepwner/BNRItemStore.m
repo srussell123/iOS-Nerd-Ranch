2ce7cd5944ce721f509cbee07af65918130a84e2
srussell123 <scottbrussell@gmail.com>
2014-06-12 12:13:47 +0900

    Ch10 finished
    Add extra view to the view stack, can now select an added item and see
    the “DetailView”

---
+++ Challenges/Ch10ChallengeHomePwner/Homepwner/Homepwner/BNRItemStore.m
@@ -0,0 +1,84 @@
+//
+//  BNRItemStore.m
+//  Homepwner
+//
+//  Created by Scott Russell on 6/10/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import "BNRItemStore.h"
+#import "BNRItem.h"
+
+@interface BNRItemStore ()
+
+@property (nonatomic) NSMutableArray *privateItems;
+
+@end
+
+@implementation BNRItemStore
+
++ (instancetype)sharedStore
+{
+    static BNRItemStore *sharedStore;
+    
+    //Do I need to create a store?
+    if (!sharedStore) {
+        sharedStore = [[self alloc] initPrivate];
+    }
+    
+    return sharedStore;
+}
+
+//If init is called let programmer know it isn't good!
+- (instancetype)init
+{
+    [NSException raise:@"Singleton"
+                format:@"Use + [BNRItemStore sharedStore"];
+    return nil;
+}
+
+//Real method
+- (instancetype)initPrivate
+{
+    self = [super init];
+    if (self) {
+        _privateItems = [[NSMutableArray alloc] init];
+    }
+    return self;
+}
+
+- (NSArray *)allItems
+{
+    return [self.privateItems copy];
+}
+
+- (BNRItem *)createItem
+{
+    BNRItem *item = [BNRItem randomItem];
+    [self.privateItems addObject:item];
+    return item;
+}
+
+- (void)removeItem:(BNRItem *)item
+{
+    [self.privateItems removeObjectIdenticalTo:item]; //similar to removeObject but no isEqual, better
+}
+
+- (void)moveItemAtIndex:(NSUInteger)fromIndex toIndex:(NSUInteger)toIndex
+{
+    if (fromIndex == toIndex) {
+        return;
+    }
+    
+    //Get pointer to object being moved so you can re-insert it
+    BNRItem *item = self.privateItems[fromIndex];
+    //Remove from array
+    [self.privateItems removeObjectAtIndex:fromIndex];
+    //Insert item in array at new location
+    [self.privateItems insertObject:item atIndex:toIndex];
+}
+
+
+
+
+@end
---
+++ Homepwner/Homepwner/BNRDetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  BNRDetailViewController.h
+//  Homepwner
+//
+//  Created by Scott Russell on 6/12/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class BNRItem;
+
+@interface BNRDetailViewController : UIViewController
+
+@property (nonatomic, strong) BNRItem *item;
+
+@end
